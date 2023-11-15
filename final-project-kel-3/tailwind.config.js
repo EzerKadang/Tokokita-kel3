8ab5cea90c46f20c78cadcee2b2f10545f347082
Willy Hardiantho <willyhardiantho22@gmail.com>
2023-11-16 02:03:20 +0800

    atur font google dan config tailwind

--- final-project-kel-3/tailwind.config.js
+++ final-project-kel-3/tailwind.config.js
@@ -2,7 +2,17 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        raleway: ["Raleway"],
+        roboto: ["Roboto"],
+      },
+      colors: {
+        primary: "#00aa5b",
+        primarybg: "#fef6f5",
+        secondarybg: "#FFEEE8",
+      },
+    },
   },
   plugins: [],
 };
