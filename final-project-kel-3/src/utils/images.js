8e760a1ace34b7086b886c42c09f150375038f91
Willy Hardiantho <willyhardiantho22@gmail.com>
2023-11-16 02:16:20 +0800

    add utils

---
+++ final-project-kel-3/src/utils/images.js
@@ -0,0 +1,9 @@
+import slider_img_1 from "../assets/images/slider_img_1.webp";
+import slider_img_2 from "../assets/images/slider_img_2.webp";
+import slider_img_3 from "../assets/images/slider_img_3.webp";
+import correct from "../assets/images/correct.png";
+import shopping_cart from "../assets/images/shopping_cart.png";
+import loader from "../assets/images/loader.svg";
+
+const sliderImgs = [slider_img_1, slider_img_2, slider_img_3];
+export { sliderImgs, correct, shopping_cart, loader };
