542181d0f222c80c3d3cc74d898832adc39127de
Willy Hardiantho <willyhardiantho22@gmail.com>
2023-11-17 12:26:39 +0800

    solve error

--- final-project-kel-3/src/App.jsx
+++ final-project-kel-3/src/App.jsx
@@ -1,4 +1,13 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import {
+  Home,
+  CategoryProduct,
+  ProductSingle,
+  Cart,
+  Search,
+  Login,
+  Register,
+} from "./pages/index";
 import Header from "./components/Header";
 import Sidebar from "./components/Sidebar";
 import Footer from "./components/Footer";
@@ -12,7 +21,15 @@ export default function App() {
         <BrowserRouter>
           <Header />
           <Sidebar />
-          <Routes></Routes>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/product/:id" element={<ProductSingle />} />
+            <Route path="/category/:category" element={<CategoryProduct />} />
+            <Route path="/cart" element={<Cart />} />
+            <Route path="/search/:searchTerm" element={<Search />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/register" element={<Register />} />
+          </Routes>
           <Footer></Footer>
         </BrowserRouter>
       </Provider>
---
+++ final-project-kel-3/src/pages/index.jsx
@@ -0,0 +1,9 @@
+import Home from "../pages/HomePage";
+import ProductSingle from "../pages/ProductSinglePage";
+import Cart from "../pages/CartPage";
+import CategoryProduct from "../pages/CategoryProductPage";
+import Search from "./SearchPage";
+import Login from "./LoginPage";
+import Register from "./RegisterPage";
+
+export { Home, ProductSingle, Cart, CategoryProduct, Search, Login, Register };
